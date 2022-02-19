a6f5deab1a35c9b1a6fe171d5a13df9143f9dcef
laxmanRavula <luckyravula123@gmail.com>
2022-02-19 15:18:42 +0000

    lAXMANCARCATELOG

---
+++ index.js
@@ -0,0 +1,177 @@
+const element = (
+  <div className="social-buttons-container">
+    <div>
+      <nav className="navbar">
+        <img
+          src="https://www.marketing91.com/wp-content/uploads/2015/03/maruti-suzuki-vector-logo.jpg"
+          alt="website-logo"
+          className="image"
+        />
+        <ul className="nav-items-list">
+          // <li className="nav-item">About</li>
+          // <li className="nav-item">Services</li>
+          // <li className="nav-item">Sales</li>
+          // <li className="nav-item">Offers</li>
+          // <li className="nav-item">Contact Us</li>
+        </ul>
+      </nav>
+    </div>
+    <div class="cards">
+      <div class="each">
+        <div>
+          <img
+            src="https://img.indianautosblog.com/2019/04/30/48428-2019-maruti-suzuki-ertiga-089c.jpg"
+            className="car"
+          />
+        </div>
+        <div className="description">
+          <h1 className="heading">Maruti Suzuki Access</h1>
+          <ul>
+            <li className="listed">
+              Price:<span className="span"> 11.00 Lakh</span>
+            </li>
+            <li className="listed">
+              Version:<span className="span"> Sigma</span>
+            </li>
+            <li className="listed">
+              Powering:<span className="span">Powering: 1.5-liter</span>
+            </li>
+            <li className="listed">
+              ManualSpeed:<span className="span"> Five-speed AT</span>
+            </li>
+          </ul>
+          <button className="button">More Details</button>
+          <br />
+          <button className="book">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left mr-2" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+            </svg>Book Here
+          </button>
+        </div>
+      </div>
+      <div class="each">
+        <div>
+          <img
+            src="https://imgd.aeplcdn.com/1056x594/cw/ec/37710/Maruti-Suzuki-Baleno-Right-Front-Three-Quarter-147420.jpg?wm=1&q=75"
+            className="car"
+          />
+        </div>
+        <div className="description">
+          <h1 className="heading">Maruti Suzuki Baleno</h1>
+          <ul>
+            <li className="listed">
+              Price:<span className="span"> 12.00 Lakh</span>
+            </li>
+            <li className="listed">
+              Version:<span className="span"> Sigma</span>
+            </li>
+            <li className="listed">
+              Powering:<span className="span">Powering: 2-liter</span>
+            </li>
+            <li className="listed">
+              ManualSpeed:<span className="span"> Five-speed AT</span>
+            </li>
+          </ul>
+          <button className="button">More Details</button>
+          <br />
+          <button className="book"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left mr-2" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+            </svg>Book Here</button>
+        </div>
+      </div>
+      <div class="each">
+        <div>
+          <img
+            src="https://imgd.aeplcdn.com/1056x594/n/cw/ec/54399/exterior-right-front-three-quarter-10.jpeg?q=75&wm=1"
+            className="car"
+          />
+        </div>
+        <div className="description">
+          <h1 className="heading">Maruti Suzuki Swift</h1>
+          <ul>
+            <li className="listed">
+              Price:<span className="span"> 15.00 Lakh</span>
+            </li>
+            <li className="listed">
+              Version:<span className="span"> Sigma</span>
+            </li>
+            <li className="listed">
+              Powering:<span className="span">Powering: 3-liter</span>
+            </li>
+            <li className="listed">
+              ManualSpeed:<span className="span"> Four-speed AT</span>
+            </li>
+          </ul>
+          <button className="button">More Details</button>
+          <br />
+          <button className="book"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left mr-2" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+            </svg>Book Here</button>
+        </div>
+      </div>
+      <div class="each">
+        <div>
+          <img
+            src="https://imgd.aeplcdn.com/1056x594/n/cw/ec/53695/new-gen-celerio-exterior-right-front-three-quarter-2.jpeg?isig=0&q=75&wm=1"
+            className="car"
+          />
+        </div>
+        <div className="description">
+          <h1 className="heading">Maruti Suzuki Celerio</h1>
+          <ul>
+            <li className="listed">
+              Price:<span className="span"> 15.00 Lakh</span>
+            </li>
+            <li className="listed">
+              Version:<span className="span"> Sigma</span>
+            </li>
+            <li className="listed">
+              Powering:<span className="span">Powering: 0.5-liter</span>
+            </li>
+            <li className="listed">
+              ManualSpeed:<span className="span"> Five-speed AT</span>
+            </li>
+          </ul>
+          <button className="button" >More Details</button>
+          <br />
+          <button className="book"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left mr-2" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+            </svg>Book Here</button>
+        </div>
+      </div>
+      <div class="each">
+        <div>
+          <img
+            src="https://imgd.aeplcdn.com/1056x594/n/cw/ec/45299/marutisuzuki-jimny-left-side-view0.jpeg?q=75&wm=1"
+            className="car"
+          />
+        </div>
+        <div className="description">
+          <h1 className="heading">Maruti Suzuki Jimny</h1>
+          <ul>
+            <li className="listed">
+              Price:<span className="span"> 20.00 Lakh</span>
+            </li>
+            <li className="listed">
+              Version:<span className="span"> Sigma</span>
+            </li>
+            <li className="listed">
+              Powering:<span className="span">Powering: 3-liter</span>
+            </li>
+            <li className="listed">
+              ManualSpeed:<spa className="span"> Five-speed AT</spa>
+            </li>
+          </ul>
+          <button className="button" >More Details</button>
+          <br />
+          <button className="book"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left mr-2" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+            </svg>Book Here</button>
+        </div>
+      </div>
+    </div>
+  </div>
+   
+);
+
+ReactDOM.render(element, document.getElementById("root"));
